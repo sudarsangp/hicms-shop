990e67d185cf4ad10e67f0df462a48445c0e1f01
sudarsangp <sudarsangp@gmail.com>
2013-09-05 00:16:17 +0800

    working version of signin and signout refer to wiki for further details

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField, validators, BooleanField
+from wtforms import TextField, PasswordField, validators, BooleanField, TextAreaField, SubmitField, ValidationError
+from models import User
 
 class LoginForm(Form):
 	userid = TextField('userid', [validators.Length(min=5, max=25),validators.Required()])
@@ -12,3 +13,45 @@ class RegisterShopForm(Form):
 	shopid = TextField('userid', [validators.Length(min=5, max=25),validators.Required()])
 	password = PasswordField('password',[validators.Required()])
 	confirmpassword = PasswordField('confirmpassword',[validators.Required()])
+
+################################################################################################################################################
+class SignupForm(Form):
+  firstname = TextField("First name",  [validators.Required("Please enter your first name.")])
+  lastname = TextField("Last name",  [validators.Required("Please enter your last name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  password = PasswordField('Password', [validators.Required("Please enter a password.")])
+  submit = SubmitField("Create account")
+ 
+  def __init__(self, *args, **kwargs):
+    Form.__init__(self, *args, **kwargs)
+ 
+  def validate(self):
+    if not Form.validate(self):
+      return False
+     
+    user = User.query.filter_by(email = self.email.data.lower()).first()
+    if user:
+      self.email.errors.append("That email is already taken")
+      return False
+    else:
+      return True
+
+class SigninForm(Form):
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  password = PasswordField('Password', [validators.Required("Please enter a password.")])
+  submit = SubmitField("Sign In")
+   
+  def __init__(self, *args, **kwargs):
+    Form.__init__(self, *args, **kwargs)
+ 
+  def validate(self):
+    if not Form.validate(self):
+      return False
+     
+    user = User.query.filter_by(email = self.email.data.lower()).first()
+    if user and user.check_password(self.password.data):
+      return True
+    else:
+      self.email.errors.append("Invalid e-mail or password")
+      return False
+#####################################################################################################################################################
--- app/view.py
+++ app/view.py
@@ -1,12 +1,12 @@
-from flask import render_template, flash
+from flask import render_template, flash, request, session, redirect, url_for
 from flask.ext.login import login_required
 from app import app, login_manager, forms
-from forms import LoginForm, RegisterShopForm
-from models import Check
+from forms import LoginForm, RegisterShopForm, SignupForm, SigninForm
+from models import Check, User, db
 
 @app.route('/check')
 def default():
-	return render_template("base layout.html")
+	return render_template("baselayout.html")
 
 #@login_manager.user_loader
 #def load_user(userid):
@@ -26,6 +26,67 @@ def login():
 def register():
 	form = RegisterShopForm();
 	return render_template('registershop.html',form = form)
+
+#############################################################################################
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  form = SignupForm()
+
+  if 'email' in session:
+  	return redirect(url_for('profile')) 
+
+  if request.method == 'POST':
+    if form.validate() == False:
+    	return render_template('signup.html', form=form)
+    else:
+    	newuser = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+      	db.session.add(newuser)
+      	db.session.commit()
+      	session['email'] = newuser.email
+     	return redirect(url_for('profile'))
+   
+  elif request.method == 'GET':
+  	return render_template('signup.html', form=form)
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+  form = SigninForm()
+
+  if 'email' in session:
+    return redirect(url_for('profile'))  
+
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('signin.html', form=form)
+    else:
+      session['email'] = form.email.data
+      return redirect(url_for('profile'))
+                 
+  elif request.method == 'GET':
+    return render_template('signin.html', form=form) 
+
+@app.route('/signout')
+def signout():
+ 
+  if 'email' not in session:
+    return redirect(url_for('signin'))
+     
+  session.pop('email', None)
+  return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():
+ 
+  if 'email' not in session:
+    return redirect(url_for('signin'))
+ 
+  user = User.query.filter_by(email = session['email']).first()
+ 
+  if user is None:
+    return redirect(url_for('signin'))
+  else:
+    return render_template('profile.html')
+##############################################################################################
 
 # to check whether this page is accesible 
 @app.route("/settings")
