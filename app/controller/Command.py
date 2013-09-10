14918074bc0b33267a459c8ae94d72b6537efd5f
Dinesh <dineshceg.nus@gmail.com>
2013-09-11 00:58:36 +0800

    Added new files

---
+++ app/controller/Command.py
@@ -0,0 +1,13 @@
+'''
+Created on Sep 10, 2013
+
+@author: dinesh
+'''
+
+class Command(object):
+    '''
+     This is an abstract class for representing commands like add, remove etc.
+     
+    '''
+    
+    
---
+++ app/controller/CommandFactory.py
@@ -0,0 +1,12 @@
+'''
+Created on Sep 10, 2013
+
+@author: dinesh
+'''
+
+class CommandFactory(object):
+    '''
+     This class determines the correct type of command and
+     returns the correct type to the caller.
+     
+    '''
