0b850498f95b761c6afb42fc93ab3afe614e3b40
unknown <vigneshr.93@gmail.com>
2013-09-25 02:56:34 +0800

    added bootstrap for base, signin and signout; need to add for addcustomer

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from flask.ext.login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+Bootstrap(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
