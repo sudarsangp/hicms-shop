284fc5e856c217ce8c30ce26f127136377b2cea3
Vignesh Rajakumar <vigneshr.93@gmail.com>
2013-10-26 12:20:59 +0800

    removed testJson and moved code to the trans file

--- app/controller/TransactionOperations.py
+++ app/controller/TransactionOperations.py
@@ -2,6 +2,9 @@ from Command import Command
 from StorageClass import StorageClass
 from Feedback import Feedback
 
+import datetime
+import json
+
 class CreateTransaction(Command):
     
     def __init__(self):
@@ -36,3 +39,24 @@ class ListTransactions(Command):
     def execute(self,formData):
         allTransactions = self.storageObject.getTransactions()
         return allTransactions     
+
+class ToJson(object):
+    def __init__(self):
+        self.storageObject = StorageClass()
+
+    def retJSON(self):
+        list1 = list()
+        list2 = list()
+
+        for row in session.query(Products).all():
+            temp = row.currentStock + row.displayQty
+            data_stock = {'Barcode' : row.barcode , 'ShopId' : '5' , 'Stock' : temp}
+            list1.append(data_stock)
+        for row1 in session.query(Products),all():
+            temp1 = session.query(func.avg(Products.price)).filter(Products.barcode = row1.barcode)
+            temp2 = session.query(Transaction.unitSold).filter(Transaction.barcode = row1.barcode and Transaction.transactionDate = datetime.datetime.now().date())
+            data_soldstock = {'Barcode' : row1.barcode, 'priceSold' : temp1, 'unitSold' : temp2, 'ShopId' : '5'}
+            list2.append(data_soldstock)
+
+        final_json = {'Stock' : list1, 'SoldStock' : list2}
+        return final_json
